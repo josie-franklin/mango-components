1bb1c22de91b8f9a8f242e25b01a7f06a3757537
dolcebasstrombone <dolcebasstrombone@gmail.com>
2024-08-22 22:19:38 -0500

    Update README and add psudocode to mangoDate

--- README.md
+++ README.md
@@ -5,3 +5,9 @@ Vanilla JS/HTML/CSS web components
 ## About
 
 This is a learning project for the web component API, with the end goal of having my own vanilla component library I can use in any project.
+
+## Learning Resources
+
+- [Simple tutorial on basic date component](https://www.freecodecamp.org/news/build-your-first-web-component/)
+- [Explanation on constructor() vs connectedCallback()](https://nolanlawson.com/2024/01/13/web-component-gotcha-constructor-vs-connectedcallback/)
+- [Shadow DOM MDN docs](https://developer.mozilla.org/en-US/docs/Web/API/Web_components/Using_shadow_DOM)
--- mangoDate.js
+++ mangoDate.js
@@ -1,21 +1,38 @@
 class MangoDate extends HTMLElement {
+  // called when element is created, example:
+  // const component = new MangoDate()
+  // can create shadow DOM, but can't add nodes to normal DOM and can't add/set attributes
+  constructor() {
+    super();
+    //set up logic for things like event listeners, subcriptions to stores, initializing state
+    // can set up shadow DOM here. Why/when would I do this? see MDN shadow DOM docs
+  }
 
-    // called when element is first added to the DOM
-    connectedCallback() {
-        const now = new Date();
-        this.textContent = now.toLocaleDateString();
-    }
+  // called when element is first added to the DOM, example:
+  // document.body.appendChild(component)
+  // best practice to set up HTML content here, as well as make requests here
+  connectedCallback() {
+    const now = new Date();
+    this.textContent = now.toLocaleDateString();
+  }
 
-    // called when element is removed from the DOM
-    disconnectedCallback() {
-        //logic
-    }
+  // called when element is removed from the DOM, example:
+  // document.body.removeChild(component)
+  // unsubscribe from events, stop timers, and unregitering callbacks should happen here
+  disconnectedCallback() {
+    //logic
+  }
 
-    // called when a watched attribute is changed
-    // watched attributes need to be added to component's static observedAttributes proprty
-    attributeChangedCallback() {
-        //logic
-    }
+  static get observedAttributed() {
+    return ["title"];
+  }
+
+  // called when a watched attribute is changed
+  // watched attributes need to be added to component's static observedAttributes proprty
+  attributeChangedCallback(name, oldVlaue, newValue) {
+    const title = this.getAttribute("title") || "";
+    this.shadowRoot.querySelector(".title").textContent = title;
+  }
 }
 
-customElements.define('mango-date', MangoDate);
+customElements.define("mango-date", MangoDate);
